6ed164b2624a80b9354ec5f35bc8a74a190becb5
Jeff Prestes <jeffprestes@gmail.com>
2019-05-18 17:45:34 -0300

    Criacao componente Home

---
+++ simulador-android-mac.md
@@ -0,0 +1,2 @@
+/usr/local/Cellar/android-sdk/24.4.1_1/emulator/emulator -list-avds
+/usr/local/Cellar/android-sdk/24.4.1_1/emulator/emulator -avd Pixel_3_API_28
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,75 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Image, Switch, ScrollView, Text, View, TouchableOpacity } from "react-native";
 
 export default class Home extends Component {
+
+  clique = () => {
+    console.log("Clique recebido");
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Bem vindo ao aplicativo dos Bombeiros</Text>
+        <View style={styles.containerTop}>
+          <View style={styles.titleTop}>
+            <Text>CORPO DE BOMBEIROS</Text>
+            <Image source={require('../images/logo1x.png')}  />          
+          </View>
+          <View>
+            <Text>Olá, Cicerelli</Text>
+            <Switch /><Text>Disponível</Text>
+          </View>
+        </View>
+        <ScrollView>
+          <View>
+            <TouchableOpacity
+              style={styles.mainButton}
+              onPress={ this.clique }
+            >
+              <Image source={require('../images/first-aid.png')}  />
+              <Text>Primeiros Socorros</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.mainButton}
+              onPress={ this.clique }
+            >
+              <Image source={require('../images/phone-call.png')}  />
+              <Text>Chamar 193</Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity
+              style={styles.mainButton}
+              onPress={ this.clique }
+            >
+              <Image source={require('../images/fire.png')}  />
+              <Text>Incêndio</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.mainButton}
+              onPress={ this.clique }
+            >
+              <Image source={require('../images/ambulance.png')}  />
+              <Text>Ambulância</Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity
+              style={styles.mainButton}
+              onPress={ this.clique }
+            >
+              <Image source={require('../images/heartbeat.png')}  />
+              <Text>Hospitais Próximos</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.mainButton}
+              onPress={ this.clique }
+            >
+              <Image source={require('../images/car-crash.png')}  />
+              <Text>Acidente</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
     );
   }
@@ -16,6 +80,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#F5FCFF",
+    flexDirection: "column"
+  },
+  titleTop : {
+    backgroundColor: "red"
+  },
+  containerTop: {
+    backgroundColor: "gray"
+  },
+  containerBody: {
+    flex: 2,
+    flexDirection: "row",
+    backgroundColor: "white"
+  },
+  mainButton: {
+    backgroundColor: "red",
+    color: "white",
+    fontFamily: "Arial",
+    fontSize: 10
   }
 });
