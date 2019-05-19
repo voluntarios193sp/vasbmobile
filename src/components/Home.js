09b0bf913577635299a3d3850f8ea92b771b7868
Jeff Prestes <jeffprestes@gmail.com>
2019-05-19 08:21:16 -0300

    Uso de Styles e ajuste de tamanho para iphone

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,14 +13,8 @@ export default class Home extends Component {
 
   clique = (page) => {
     console.log('Clique recebido ' + page);
-
     return this.props.navigation.navigate(page, { });
-  }
-
-  abreCadastro = () => {
-    console.log("Clique abreCadastro");
-    this.props.fnChangeWindow("CADASTRO");
-  }
+  }  
 
   render() {
     return (
@@ -35,43 +29,43 @@ export default class Home extends Component {
         </View>
         <View style={styles.containerBody}>        
             <TouchableOpacity style={styles.mainButton} onPress={ () => { this.clique('PrimeirosSocorros') } }>
-              <Icon name="first-aid" color="rgba(255, 255, 255, 1)" size={90} />
+              <Icon name="first-aid" style={styles.iconMain}/>
               <Text style={styles.textButton}>Primeiros Socorros</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.mainButton} onPress={ () => { this.clique('Incendio') } }>
-              <Icon name="phone-volume" color="rgba(255, 255, 255, 1)" size={90} />
+              <Icon name="phone-volume" style={styles.iconMain} />
               <Text style={styles.textButton}>Chamar 193</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.mainButton} onPress={ () => { this.clique('Incendio') } }>
-              <Icon name="fire" color="rgba(255, 255, 255, 1)" size={90} />
+              <Icon name="fire" style={styles.iconMain} />
               <Text style={styles.textButton}>Incêndio</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.mainButton} onPress={ () => { this.clique('Emergencia') } }>
-              <Icon name="ambulance" color="rgba(255, 255, 255, 1)" size={90} />
+              <Icon name="ambulance" style={styles.iconMain} />
               <Text style={styles.textButton}>Ambulância</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.mainButton} onPress={ () => { this.clique('Hospitais') } }>
-              <Icon name="heartbeat" color="rgba(255, 255, 255, 1)" size={90} />
+              <Icon name="heartbeat" style={styles.iconMain} />
               <Text style={styles.textButton}>Hospitais Próximos</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.mainButton} onPress={ () => { this.clique('Emergencia') } }>
-              <Icon name="car-crash" color="rgba(255, 255, 255, 1)" size={90} />
+              <Icon name="car-crash" style={styles.iconMain} />
               <Text style={styles.textButton}>Acidente</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.mainButton} onPress={ () => { this.clique('Perfil') } }>
-              <Icon name="id-card" color="rgba(255, 255, 255, 1)" size={90} />
+              <Icon name="id-card" style={styles.iconMain} />
               <Text style={styles.textButton}>Meu Perfil</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.mainButton} onPress={ () => { this.clique('Calendario') } }>
-              <Icon name="calendar-alt" color="rgba(255, 255, 255, 1)" size={90} />
+              <Icon name="calendar-alt" style={styles.iconMain} />
               <Text style={styles.textButton}>Calendario</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.mainButton} onPress={ () => { this.clique('Doacao') } }>
-              <Icon name="hand-holding-usd" color="rgba(255, 255, 255, 1)" size={90} />
+              <Icon name="hand-holding-usd" style={styles.iconMain} />
               <Text style={styles.textButton}>Doações</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.mainButton} onPress={ () => { this.clique('Informacao') } }>
-              <Icon name="info-circle" color="rgba(255, 255, 255, 1)" size={90} />
+              <Icon name="info-circle" style={styles.iconMain} />
               <Text style={styles.textButton}>Informação</Text>
             </TouchableOpacity>
         </View>        
@@ -113,7 +107,7 @@ const styles = StyleSheet.create({
     backgroundColor: "red",
     textAlign: "center",
     alignItems: "center",
-    padding: 0,
+    padding: 10,
     marginBottom: 10,
     width: "48%"   
   },
@@ -122,5 +116,9 @@ const styles = StyleSheet.create({
     fontFamily: "Arial",
     fontSize: 14,
     fontWeight: "bold"
+  },
+  iconMain : {
+    color : "rgba(255, 255, 255, 1)",
+    fontSize : 65,
   }
 });
